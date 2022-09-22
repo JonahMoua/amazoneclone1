6727e8cd0f241f6dec503273e14c9964662d15d2
jmoua123 <nraugnaagmouaa@gmail.com>
2022-09-21 17:12:14 -0700

    Reverted

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,36 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51LgNmuJIcvtPvKwccNp6OzTK3tQ12OTKjupB4t1tE2XVIDz4UwhbfkaVfykuUEX5iTz1IbPSt8PP94NuWt5BlC1e00dc0tINHR"
+);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+
+const app = express();
+
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, 
+    currency: "usd",
+  });
+
+
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+
+exports.api = functions.https.onRequest(app);
